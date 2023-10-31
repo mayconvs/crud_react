9bb16564af421d077ca9a3e47174bd85694893fd
Maycon <mayconvsg@gmail.com>
2023-10-31 07:25:05 -0300

    Arrumando para funcionar em localhost

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-//const baseURL = "http://localhost:8080";
-const baseURL = "https://teste-api-bistek3-942ad9b95822.herokuapp.com";
+const baseURL = "http://localhost:8080";
+//const baseURL = "https://teste-api-bistek3-942ad9b95822.herokuapp.com";
 
 export default baseURL;
